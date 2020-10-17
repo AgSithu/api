a52a21e2dffca623db729d366125b21c7b096a5e
Sithu Aung <sithuaung@gmail.com>
2020-10-17 09:59:46 +0630

    Migration created for product & reviews

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ReviewController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::apiResource('/products', ProductController::class);
+
+Route::group(['prefix'=>'products'], function(){
+    Route::apiResource('/{product}/reviews', ReviewController::class);
 });
